b83df1bdf480ff9ab2fc26fecd388a28923405ac
Tesseract <tensai3355@gmail.com>
2022-02-11 03:54:12 +0900

    :lipstick: Fix NotFound page layout

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,7 +1,19 @@
+import styled from 'styled-components';
 const { HomeHeader } = require("./Home")
 
+const Container = styled.section`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
 const NotFound = () => {
-  return <HomeHeader>Not Found</HomeHeader>;
+  return (
+    <Container>
+      <HomeHeader>Not Found</HomeHeader>
+    </Container>
+  );
 };
 
 export default NotFound;
