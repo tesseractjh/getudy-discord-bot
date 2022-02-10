f9de6299bf2d4da759bf10312567df02786e607a
Tesseract <tensai3355@gmail.com>
2022-02-11 02:25:25 +0900

    :truck: Modify path

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 bot.server();
-app.use(express.static(path.resolve(__dirname, './build')));
+app.use(express.static(path.resolve(__dirname, './dist')));
 app.use(express.json());
 
 app.use('/api', linkRouter);
 app.use('/api', emojiRouter);
 app.use('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build/index.html'));
+  res.sendFile(path.resolve(__dirname, './dist/index.html'));
 });
 
 app.listen(PORT, () => console.log(`👂 listening on ${PORT}`));
